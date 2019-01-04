7f75f2821655d82a4b3c7bb5acff391f51cbf655
allemanfredi <alle.manfredi@gmail.com>
2019-01-04 15:06:38 +0100

    header nodes selection view and controller

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -39,38 +39,32 @@ class Header extends Component {
               <img src="./material/logo/pegasus-64.png" height="40" width="40"/>
             </div>
             <div class="container-selection">
-              <div class="network-selection">
-                <div class="row">
-                  <div class="col-10 text-center">
-                    <div class="selected-network">
-                      {this.state.network.name}
-                    </div>
-                  </div>
-                  <div class="col-2 text-center">
-                    <button onClick={e => {this.setState({showNetworks : !this.state.showNetworks})}} class="btn btn-show-network"><span class="fa fa-chevron-down"></span></button>
-                  </div>
+                <div class="container-signal-icon">
+                  <i class="fa fa-signal"></i>
                 </div>
-              </div>
+                <div class="selected-network">
+                  {this.state.network.name}
+                </div>
+                <div onClick={e => {this.setState({showNetworks : !this.state.showNetworks})}} class="btn-show-hidden-network">
+                  { this.state.showNetworks ? <span class="fa fa-chevron-up"></span>  : <span class="fa fa-chevron-down"></span> }
+                </div>
             </div>
             { this.state.showNetworks ? 
                 <div class="container-hidden-network">
-                  <div class="container-hidden-network-header">Networks</div>
+                  <div class="container-hidden-network-header">Nodes</div>
                   <div class="container-hidden-network-body">
                     {options.network.map( (network,index) => {
                       return(
                         <div onClick={() => this.switchNetwork(network)} class="container-hidden-network-item">
-                          <div class="row">
-                            <div class="col-2 text-center">
+                            
+                            <div class="container-icon-check">
                               { this.state.network.id === network.id ? 
                                 <span class="fa fa-check"></span>
                               :''}
                             </div>
-                            <div class="col-10 text-left">
-                              { this.state.network.id === network.id ? 
-                                <div class="container-hidden-network-item-name-selected">{network.name}</div>
-                              : <div class="container-hidden-network-item-name-not-selected">{network.name}</div>}
-                            </div>
-                          </div>
+                            { this.state.network.id === network.id ? 
+                              <div class="container-hidden-network-item-name-selected">{network.name}</div>
+                            : <div class="container-hidden-network-item-name-not-selected">{network.name}</div>}
                         </div>
                       )
                       })}
