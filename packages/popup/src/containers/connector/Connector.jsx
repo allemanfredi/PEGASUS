52fa3cad967da58c743502ca1755d18de61ffe5d
allemanfredi <alle.manfredi@gmail.com>
2019-10-30 09:38:12 +0100

    chore(popup): refactors connector page

--- packages/popup/src/containers/connector/Connector.jsx
+++ packages/popup/src/containers/connector/Connector.jsx
@@ -6,7 +6,19 @@ class Connector extends Component {
   constructor(props, context) {
     super(props, context)
 
-    this.state = {}
+    this.state = {
+      favicon: null,
+      hostname: ''
+    }
+  }
+
+  async componentDidMount() {
+    const website = await PopupAPI.getWebsite()
+    console.log(website)
+    this.setState({
+      favicon: website.favicon,
+      hostname: website.hostname
+    })
   }
 
   render() {
@@ -21,27 +33,31 @@ class Connector extends Component {
         <hr className="mt-2 mb-2" />
         <div className="row">
           <div className="col-4 text-center">
-            <img className="border-radius-50" src='./material/logo/pegasus-128.png' height='80' width='80' alt='pegasus logo'/>
+            <img className="border-radius-50"
+              src={this.state.favicon ? this.state.favicon : './material/img/domain.png'} 
+              height='64'
+              width='64'
+              alt='website logo'/>
           </div>
           <div className="col-4 text-center my-auto">
-            <img src='./material/img/broken-link.png' height='50' width='50' alt='broken-link logo'/>
+            <img src='./material/img/broken-link.png' height='30' width='30' alt='broken-link logo'/>
           </div>
           <div className="col-4 text-center">
-            <img className="border-radius-50" src='./material/logo/pegasus-128.png' height='80' width='80' alt='pegasus logo'/>
+            <img className="border-radius-50" src='./material/logo/pegasus-128.png' height='64' width='64' alt='pegasus logo'/>
           </div>
         </div>
         <div className="row mt-05">
-          <div className="col-4 text-center text-xs text-bold">
-            origin
+          <div className="col-4 text-center text-xxs text-bold">
+            {this.state.hostname}
           </div>
-          <div className="col-4 text-center text-xxs my-auto pl-0 pr-0 text-gray">
+          <div className="col-4 text-center text-xxxs my-auto pl-0 pr-0 text-gray">
             wants to connect with
           </div>
-          <div className="col-4 text-center text-xs text-bold">
+          <div className="col-4 text-center text-xxs text-bold">
             Pegasus
           </div>
         </div>
-        <div className="row mt-4">
+        <div className="row mt-5">
           <div className="col-12 text-center text-md text-bold">
             Are you sure you want to enable the connection with this website?
           </div>
@@ -51,7 +67,7 @@ class Connector extends Component {
             allowing, the website will be able to safely interact with the Wallet thanks to the Pegasus Connector!
           </div>
         </div>
-        <div className="row mt-11">
+        <div className="row mt-12">
           <div className="col-6 pl-5 pr-5">
             <button onClick={() => this.props.onPermissionNotGranted()} className="btn btn-border-blue text-sm text-bold btn-big">Reject</button>
           </div>
